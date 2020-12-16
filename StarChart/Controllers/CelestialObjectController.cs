0062a4587f69d6be09765f5b7acaa17e163b6e55
Shawn McGee <shawn.mcgee@canal-ins.com>
2020-12-16 12:25:53 -0500

    Create CelestialObjectController's Get Actions - SMcGee 12/16/20

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -8,7 +8,7 @@ using StarChart.Data;
 namespace StarChart.Controllers
 {
     [Route("")]
-    [ApiControllerAttribute]
+    [ApiController]
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -16,6 +16,40 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if(celestialObject == null)
+                return NotFound();
+            celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObject.Id == id).ToList();
+            return Ok(celestialObject);
+        }
+           
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjectName = _context.CelestialObjects.Where(e => e.Name == name);
+            if (celestialObjectName == null)
+                return NotFound();
+            foreach (var celestialObject in celestialObjectName)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObject.Id == celestialObject.Id).ToList();
+            }           
+            return Ok(celestialObjectName);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allCelestialObjects = _context.CelestialObjects.ToList();
+            foreach(var celestialObject in allCelestialObjects)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObject.Id == celestialObject.Id).ToList();
+            }
+            return Ok(allCelestialObjects);
         }
     }
 }
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -10,10 +10,10 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int Id { get; set; }
-        [Required()]
+        [Required]
         public string Name { get; set; }
-        public int? OrbitedObjectId { get; set; }
-        [NotMapped()]
+        public CelestialObject OrbitedObject { get; set; }
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
 
